29763af3d8e849619a11d64acbd9146861780a69
JaapBeetstra <jaap@app-it-up.com>
2021-04-17 13:37:41 +0200

    eerste versie

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# boter-kaas-eieren
+# Boter Kaas en Eieren
+
 Simpele HTML/CSS/JavaScript versie van boter, kaas en eieren (AKA Tic-tac-toe)
+
+Er zijn twee versies, een voor 2 spelers om de beurt, en een voor 1 speler tegen de computer. De twee versies delen de
+CSS-stylesheet.
+
+## HTML/JavaScript
+
+De meest simpele versie is voor 2 spelers, en is te vinden in de bestanden
+
+* tweeSpelers.html
+* tweeSpelers.js
+
+Er is ook een versie voor 1 speler tegen de computer, die is te vinden in de bestanden
+
+* eenSpeler.html
+* eenSpeler.js
+
+Deze twee versies bevatten nu geen gedeelde JavaScript-bestanden, ze zijn geheel onafhankelijk. Wel zijn ze op een
+vergelijkbare manier opgebouwd. De opbouw is daarbij bewust rechttoe-rechtaan gehouden, zodat het niet noodzakelijk is
+ingewikkelder concepten en structuren te kennen om de code te begrijpen.
+
+De computerspeler gebruikt een simpel recursief algoritme om de beste zet te vinden. Dat betekent wel dat de speler
+kansloos is om te winnen; het best mogelijke resultaat is een gelijkspel.
+
+## CSS
+
+Er is een basis CSS, `boter-kaas-eieren-simpel.css` die alleen de meest basale opmaak bevat,
+en `boter-kaas-eieren-fancy.css` die een wat mooiere opmaak bevat met o.a. ook CSS-transities.
+
---
+++ eenSpeler.js
@@ -0,0 +1,153 @@
+let computerTeken = 'O';
+let spelerTeken = 'X';
+let bord = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+let spelAfgelopen = false;
+let alleStatussen = ['status_begin', 'status_speler_aan_zet', 'status_computer_wint',
+	'status_speler_gewonnen', 'status_computer_gewonnen', 'status_gelijkspel'];
+let status = 'status_begin';
+
+function pak(id) {
+	return document.getElementById(id);
+}
+
+function nieuwSpel() {
+	const computerBegint = pak('computerBegint').checked;
+	if (computerBegint) {
+		computerTeken = 'X';
+		spelerTeken = 'O';
+	} else {
+		computerTeken = 'O';
+		spelerTeken = 'X';
+	}
+	bord = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+	spelAfgelopen = false;
+	if (computerBegint)
+		doeZet(0, 0, computerTeken);
+	status = 'status_speler_aan_zet';
+	beeldSpelAf();
+}
+
+function beeldSpelAf() {
+	for (let rij = 0; rij < 3; rij++)
+		for (let kolom = 0; kolom < 3; kolom++) {
+			let vakje = pak("rij_" + rij + "_kolom_" + kolom);
+			vakje.innerText = bord[rij][kolom];
+			vakje.className = bord[rij][kolom];
+		}
+	for (let i = 0; i < alleStatussen.length; i++)
+		pak(alleStatussen[i]).className = '';
+	pak(status).className = 'zichtbaar';
+}
+
+function isWinnaar(teken) {
+	for (let kolom = 0; kolom < 3; kolom++)
+		if (isKolomWinnaar(kolom, teken))
+			return true;
+	for (let rij = 0; rij < 3; rij++)
+		if (isRijWinnaar(rij, teken))
+			return true;
+	if (isDiagonaalLinksBovenRechtsOnderWinnaar(teken))
+		return true;
+	if (isDiagonaalLinksOnderRechtsBovenWinnaar(teken))
+		return true;
+	return false;
+}
+
+function isKolomWinnaar(kolom, teken) {
+	return bord[0][kolom] === teken && bord[1][kolom] === teken && bord[2][kolom] === teken;
+}
+
+function isRijWinnaar(rij, teken) {
+	return bord[rij][0] === teken && bord[rij][1] === teken && bord[rij][2] === teken;
+}
+
+function isDiagonaalLinksBovenRechtsOnderWinnaar(teken) {
+	return bord[0][0] === teken && bord[1][1] === teken && bord[2][2] === teken;
+}
+
+function isDiagonaalLinksOnderRechtsBovenWinnaar(teken) {
+	return bord[0][2] === teken && bord[1][1] === teken && bord[2][0] === teken;
+}
+
+function isBordVol() {
+	for (let rij = 0; rij < 3; rij++)
+		for (let kolom = 0; kolom < 3; kolom++)
+			if (bord[rij][kolom] === ' ')
+				return false;
+	return true;
+}
+
+function verwerkZet() {
+	if (isWinnaar(spelerTeken)) {
+		spelAfgelopen = true;
+		status = 'status_speler_gewonnen';
+	} else if (isWinnaar(computerTeken)) {
+		spelAfgelopen = true;
+		status = 'status_computer_gewonnen';
+	} else if (isBordVol()) {
+		spelAfgelopen = true;
+		status = 'status_gelijkspel';
+	} else {
+		spelAfgelopen = false;
+		if (zoekBesteZet(spelerTeken).score < 0)
+			status = 'status_computer_wint';
+		else
+			status = 'status_speler_aan_zet';
+	}
+}
+
+function veldGekozen(rij, kolom) {
+	if (spelAfgelopen)
+		return;
+	if (bord[rij][kolom] !== ' ')
+		return;
+	doeZet(rij, kolom, spelerTeken);
+	if (!spelAfgelopen)
+		doeComputerZet();
+	beeldSpelAf();
+}
+
+function doeComputerZet() {
+	const zet = zoekBesteZet(computerTeken);
+	doeZet(zet.rij, zet.kolom, computerTeken);
+}
+
+function doeZet(rij, kolom, teken) {
+	bord[rij][kolom] = teken;
+	verwerkZet();
+}
+
+function zoekBesteZet(teken) {
+	let besteZet = {score: -2};
+	for (let rij = 0; rij < 3; rij++)
+		for (let kolom = 0; kolom < 3; kolom++)
+			if (bord[rij][kolom] === ' ') {
+				const zet = {rij: rij, kolom: kolom};
+				zet.score = berekenScoreVoorZet(zet, teken);
+				if (zet.score > besteZet.score)
+					besteZet = zet;
+			}
+	return besteZet;
+}
+
+function berekenScoreVoorZet(zet, teken) {
+	bord[zet.rij][zet.kolom] = teken;
+	let score;
+	if (isWinnaar(teken)) {
+		score = 1;
+	} else if (isBordVol()) {
+		score = 0;
+	} else {
+		const volgendeZet = zoekBesteZet(ander(teken));
+		score = -volgendeZet.score;
+	}
+	bord[zet.rij][zet.kolom] = ' ';
+	return score;
+}
+
+function ander(teken) {
+	if (teken === 'X')
+		return 'O';
+	else
+		return 'X';
+}
